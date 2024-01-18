6e68be33c708c4163252c8f830934d7d1c9689fb
N0zzy <ffnn83@yandex.ru>
2024-01-18 19:00:34 +0500

    0.6.3 fixed: args for net8.0

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using PhpieSdk.Library;
 using PhpieSdk.Library.Service;
 
@@ -8,20 +9,43 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        new PhpScriptGenerator(new Settings()
+        new PhpieSdkRunner().Benchmark();
+    }
+    
+}
+public class PhpieSdkRunner
+{
+    public void Run()
+    {
+        new PhpScriptGenerator(new Settings() 
         {
             currentPath = Directory.GetCurrentDirectory().ToReversSlash(),
-            
+        
             targetBuild = "Debug",
             targetFramework = "net7.0",
-            
+        
             sdkName = ".sdk",
             sdkPath = ".sdkpath",
             sdkIgnore = ".sdkignore",
+        
+            isMakeSdkList = true,
+            isMakeSdkFiles = true,
             
-            isMakeSdkList = true
-        })
-        .Execute();
+            isViewLibsIgnore = true,
+            isViewOutputPath = true,
+            isViewLibsLoaded = true
+            
+        }).Execute();
+    }
+
+    public void Benchmark()
+    {
+        var t0 = new DateTimeOffset(DateTime.Now);
+        Run();
+        var t1 = new DateTimeOffset(DateTime.Now);
+        "".BenchmarkWriteLn("Start:  " + t0.Hour + ":" + t0.Minute + ":" + t0.Second + "." + t0.Millisecond);
+        "".BenchmarkWriteLn("Finish: " + t1.Hour + ":" + t1.Minute + ":" + t1.Second + "." + t1.Millisecond);
+        var diff = t1 - t0;
+        "".BenchmarkWriteLn("Total:  " + Math.Round(diff.TotalMilliseconds / 1000, 2) + " sec. [" +  diff.TotalMilliseconds + " ms.]");
     }
 }
-
